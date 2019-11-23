fbc5b7f165e4a63f2d8607a8e5f515710ab3b769
caijw <285025525@qq.com>
2019-11-23 23:50:31 +0800

    feat: update

--- src/calculator/index.ts
+++ src/calculator/index.ts
@@ -1,9 +1,11 @@
 import { Lexer } from "./Lexer";
 import { TokenReader } from "./Token";
+import { Calculator } from "./Calculator";
 
 let script: string;
 let tokenReader: TokenReader;
 const lexer = new Lexer();
+const calculator: Calculator = new Calculator();
 
 script = "int age = 45; i";
 console.log(`parse: "${script}"`);
@@ -30,3 +32,14 @@ console.log(`parse: "${script}"`);
 tokenReader = lexer.tokenize(script);
 lexer.dump(tokenReader);
 
+script = "2+3*5";
+console.log("计算: " + script + "，看上去一切正常。");
+calculator.evaluateScript(script);
+
+script = "2+";
+console.log(script + "，应该有语法错误。");
+calculator.evaluateScript(script);
+
+script = "2+3+4";
+console.log("计算: " + script + "，结合性出现错误。");
+calculator.evaluateScript(script);
